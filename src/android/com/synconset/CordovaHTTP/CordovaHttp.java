ed22c42e62277722fb93e7a97e500b77a971029d
mourice <mouricef@mourices-imac.mumbai1.corp.citiustech.com>
2017-09-11 15:20:30 +0530

    Android import issue fixed

--- src/android/com/synconset/CordovaHTTP/CordovaHttp.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttp.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import java.net.MalformedURLException;
 import java.net.URL;
